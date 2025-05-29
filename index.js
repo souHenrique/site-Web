d2303485ff05fc8b04f8b449583e802451470c0d
souHenrique <luishenriq.182@gmail.com>
2025-05-29 11:49:07 -0300

    início aplicação do JS, adicionado botão para retornar ao topo

---
+++ index.js
@@ -0,0 +1,7 @@
+const botaoTopo = document.getElementById("botao-topo");
+
+botaoTopo.onclick = function(){
+    window.scrollTo({top: 0});
+}
+
+//alert("Bem-vindo à Clínica RM! Explore nossos serviços e agende sua consulta.");
