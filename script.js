659d6e6a091d455841674a764b1c27b87667f698
souHenrique <luishenriq.182@gmail.com>
2025-05-31 09:55:08 -0300

    adicionado função de slides manual e auto na aba de serviços

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const botaoTopo = document.getElementById("botao-topo");
+let count = 1;
+
+alert("Bem-vindo à Clínica RM! Explore nossos serviços e agende sua consulta.");
 
 botaoTopo.onclick = function(){
     window.scrollTo({top: 0});
 }
 
-//alert("Bem-vindo à Clínica RM! Explore nossos serviços e agende sua consulta.");
+document.getElementById("radio1-serv").checked = true;
+setInterval(function(){
+    nextImage();
+}, 5000);
+function nextImage(){
+    count++;
+    if(count > 4){
+        count = 1;
+    }
+    document.getElementById("radio" + count + "-serv").checked = true;
+}
